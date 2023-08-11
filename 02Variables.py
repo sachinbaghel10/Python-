40db9bd4fb33491cff3bf527df8dd22ac761eba1
Sachin Baghel <sachinbaghel43@gmail.com>
2023-08-11 17:26:35 +0530

    adding variable in file

---
+++ 02Variables.py
@@ -0,0 +1,23 @@
+# commants 
+''' 
+This for multi line commants ....
+'''
+
+# Variables
+a = 4 
+print(a)
+b = 30 
+print(b)
+a = 434.234
+print(a)
+#boolean 
+c = True # or false
+print(c)
+#string 
+d = "Sachin"
+print(d)
+
+e = None
+print(e)
+
+
