89cc8c4ebbf24890d868d2d1567e19f2a80be076
Sachin Baghel <sachinbaghel43@gmail.com>
2023-09-22 11:59:26 +0530

    if

---
+++ 11_if_.py
@@ -0,0 +1,47 @@
+a = 10 
+b = 30 
+if a > b :
+    print("a is greater than b")
+    # Example: Write a program to check if the given number is even
+
+else :
+
+    print ("b is greater or equal to A ")
+    #Example :Write a program that takes two numbers and prints their sum. If one of them is
+
+m  =10 
+n = 20 
+v = 40 
+if (m > n) & (m > v ) :
+    print("m is greater then n and v")
+if (n > m ) & ( n > v):
+    print( "n is greater then m and v" )
+else:
+    print(" v is greater then m and n ")
+    
+#if with tuple 
+
+tup1 = (3 , 45, "sachin")
+if  "hello" in tup1:
+    print('yes')
+else: 
+    print("No")   
+
+#if with list
+l1 = [ 'a' , 'b' , 'c']
+print(l1)
+print("before changing the list :  ")
+
+
+if l1[1] == 'b' :
+    l1[1]='sachin'
+    print(l1)
+    
+#if with dictionary
+
+dic = { 'apple':12 , 'mango':39}
+if ('apple' > 'mango') :
+    print("mango prince is lesser")
+else : 
+    print("mango price is greater ")
+    
