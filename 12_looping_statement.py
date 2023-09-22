85d2d459fecad3f4201363427a018e3909f9970e
Sachin Baghel <sachinbaghel43@gmail.com>
2023-09-22 12:05:32 +0530

    looping statement

---
+++ 12_looping_statement.py
@@ -0,0 +1,13 @@
+#Looping Statements
+#Looping statement are used to repeat a tast multiple times 
+for i in range(2):
+    print("Hello World")
+    print('Done')
+    
+    
+#while
+i=0;
+while (i<5) :
+    print ("hello world");
+    print ('done');
+    
