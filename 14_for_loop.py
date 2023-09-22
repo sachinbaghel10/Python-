a19b18b93186c1a9ef77cdc79da0c38edd0507e2
Sachin Baghel <sachinbaghel43@gmail.com>
2023-09-22 12:59:06 +0530

    for loop

---
+++ 14_for_loop.py
@@ -0,0 +1,11 @@
+list1= ['mango ' , 'apple' , 'guava']
+for i in list1:
+    print(i)
+
+l1 = ['house' , 'garden' , 'outside']    
+l2 = ['chair' , 'grass' , ' car']
+for  i in l1: 
+    for j in l2 :
+        print(i , j)
+        
+        
